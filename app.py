aef14ea5e01f017b171435aac80b956c47c2159b
09072006Ali <69715758+09072006Ali@users.noreply.github.com>
2026-05-28 18:39:58 +0200

    Add kilometers to miles converter

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ def celsius_to_fahrenheit(celsius):
 
 if __name__ == "__main__":
     print(f"20 Santigrat = {celsius_to_fahrenheit(20)} Fahrenhayt")
+
+
+    def km_to_miles(km):
+        """Converts Kilometers to Miles."""
+        return km * 0.621371
--- test_app.py
+++ test_app.py
@@ -1,11 +1,14 @@
 import unittest
-from app import celsius_to_fahrenheit
-
+from app import celsius_to_fahrenheit, km_to_miles
 class TestConverter(unittest.TestCase):
     def test_celsius_to_fahrenheit(self):
         # Doğrulama: 20 derece 68 Fahrenhayt olmalıdır
         self.assertEqual(celsius_to_fahrenheit(20), 68.0)
         self.assertEqual(celsius_to_fahrenheit(0), 32.0)
+
+    def test_km_to_miles(self):
+        # Verification: 10 km should be approximately 6.21371 miles
+        self.assertAlmostEqual(km_to_miles(10), 6.21371, places=5)
 
 if __name__ == '__main__':
     unittest.main()

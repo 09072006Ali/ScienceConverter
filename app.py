2544bbc25b3fcaf27fc22d0322283cde8fc261a6
09072006Ali <69715758+09072006Ali@users.noreply.github.com>
2026-05-28 19:35:48 +0200

    Add alternative km to miles converter

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ def celsius_to_fahrenheit(celsius):
 
 if __name__ == "__main__":
     print(f"20 Santigrat = {celsius_to_fahrenheit(20)} Fahrenhayt")
+
+
+def km_to_miles(km):
+    # Lider bu formülü farklı biliyor (Kasıtlı Hata/Çakışma)
+    return km * 0.62
